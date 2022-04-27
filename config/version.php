632e303555b1b2a040221102f3ae404714ac9833
GorillaBearWolf <mrefsnid@fau.edu>
2022-04-27 12:16:25 -0400

    first commit

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v5.4.3',
+  'full_app_version' => 'v5.4.3 - build 6812-g7479f5f12',
+  'build_version' => '6812',
+  'prerelease_version' => '',
+  'hash_version' => 'g7479f5f12',
+  'full_hash' => 'v5.4.3-5-g7479f5f12',
+  'branch' => 'master',
+);
---
+++ database/migrations/2018_10_19_153910_add_kits_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddKitsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if (!Schema::hasTable('kits')) {
+			Schema::create('kits', function ($table) {
+				$table->increments('id');
+				$table->string('name')->nullable()->default(NULL);
+				$table->timestamps();
+				$table->engine = 'InnoDB';
+			});
+		}
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		if (Schema::hasTable('kits')) {
+			Schema::drop('kits');
+		}
+
+	}
+
+}
